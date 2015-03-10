b3fc539f497d94d26ede472e8f4e0251d5e4afc9
Jim Teeuwen <jimteeuwen@gmail.com>
2015-03-10 23:11:38 +0100

    Adds `FunctionParamAttr` constants.
    Signed-off-by: Jim Teeuwen <jimteeuwen@gmail.com>

--- constant.go
+++ constant.go
@@ -688,3 +688,73 @@ func (s StorageClass) String() string {
 
 	return fmt.Sprintf("StorageClass(%d)", uint32(s))
 }
+
+// FunctionParamAttr adds additional information to the return type
+// and to each parameter of a function.
+type FunctionParamAttr uint32
+
+// Known function parameter attributes.
+const (
+	// Value should be zero extended if needed.
+	FPAZext FunctionParamAttr = 0
+
+	// Value should be sign extended if needed.
+	FPASext FunctionParamAttr = 1
+
+	// This indicates that the pointer parameter should really be passed by
+	// value to the function. Only valid for pointer parameters (not
+	// for ret value)
+	FPAByVal FunctionParamAttr = 2
+
+	// Indicates that the pointer parameter specifies the address of a
+	// structure that is the return value of the function in the source
+	// program. Only applicable to the first parameter which must be a
+	// pointer parameters.
+	FPASret FunctionParamAttr = 3
+
+	// Indicates that the memory pointed by a pointer parameter is not
+	// accessed via pointer values which are not derived from this pointer
+	// parameter. Only valid for pointer parameters. Not valid on return values
+	FPANoAlias FunctionParamAttr = 4
+
+	// The callee does not make a copy of the pointer parameter into a
+	// location that is accessible after returning from the callee. Only
+	// valid for pointer parameters. Not valid on return values.
+	FPANoCapture FunctionParamAttr = 5
+
+	// To be determined.
+	FPASVM FunctionParamAttr = 6
+
+	// Can only read the memory pointed by a pointer parameter.
+	// Only valid for pointer parameters. Not valid on return values.
+	FPANoWrite = 7
+
+	// Cannot dereference the memory pointed by a pointer parameter.
+	// Only valid for pointer parameters. Not valid on return values.
+	FPANoReadWrite = 8
+)
+
+func (fpa FunctionParamAttr) String() string {
+	switch fpa {
+	case FPAZext:
+		return "Zero Extend"
+	case FPASext:
+		return "Sign Extend"
+	case FPAByVal:
+		return "By Value"
+	case FPASret:
+		return "Struct Address"
+	case FPANoAlias:
+		return "No Alias"
+	case FPANoCapture:
+		return "No Capture"
+	case FPASVM:
+		return "SVM"
+	case FPANoWrite:
+		return "No Write"
+	case FPANoReadWrite:
+		return "No Read/Write"
+	}
+
+	return fmt.Sprintf("FunctionParamAttr(%d)", uint32(fpa))
+}
