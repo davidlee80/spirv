88c90ad367ee3e6a56f36bc731062b4c83014266
Jim Teeuwen <jimteeuwen@gmail.com>
2015-03-10 21:53:06 +0100

    Adds LinkageType constants.
    Signed-off-by: Jim Teeuwen <jimteeuwen@gmail.com>

---
+++ lib/linkagetype.go
@@ -0,0 +1,28 @@
+// This file is subject to a 1-clause BSD license.
+// Its contents can be found in the enclosed LICENSE file.
+
+package lib
+
+import "fmt"
+
+// LinkageType associates a linkage type to functions or global
+// variables. By default, functions and global variables are private
+// to a module and cannot be accessed by other modules.
+type LinkageType uint32
+
+// Known execution models.
+const (
+	LinkExport LinkageType = 0 // Accessible by other modules as well.
+	LinkImport LinkageType = 1 // Declaration for a global identifier that exists in another module.
+)
+
+func (e LinkageType) String() string {
+	switch e {
+	case LinkExport:
+		return "Export"
+	case LinkImport:
+		return "Import"
+	}
+
+	return fmt.Sprintf("LinkageType(%d)", uint32(e))
+}
