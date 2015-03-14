1c3fc8b0271e29f833ff6afbaa268ca2e48ed6bb
Jim Teeuwen <jimteeuwen@gmail.com>
2015-03-14 14:12:34 +0100

    Reintroduces the `Module` type. It provides the highest level abstracting in this package and operates on complete module data.
    The most notable feature which will be fleshed out in the
    coming time, is the semantic validation of a full package.
    This applies all rules specified in the spec to ensure the
    code doesn't do anything naughty.
    
    Signed-off-by: Jim Teeuwen <jimteeuwen@gmail.com>

---
+++ module.go
@@ -0,0 +1,101 @@
+// This file is subject to a 1-clause BSD license.
+// Its contents can be found in the enclosed LICENSE file.
+
+package spirv
+
+import "io"
+
+// Module defines a complete SPIR-V module.
+type Module struct {
+	Header Header
+	Code   []Instruction
+}
+
+// NewModule creates a new, default module.
+func NewModule() *Module {
+	return &Module{
+		Header: Header{
+			Magic:          MagicLE,
+			Version:        SpecificationVersion,
+			GeneratorMagic: 0,
+			Bound:          0,
+			Reserved:       0,
+		},
+	}
+}
+
+// Load loads a full module from the given input stream.
+func Load(r io.Reader) (*Module, error) {
+	var mod Module
+	var err error
+	var instr Instruction
+
+	dec := NewDecoder(r)
+
+	// Load the module header.
+	mod.Header, err = dec.DecodeHeader()
+	if err != nil {
+		return nil, err
+	}
+
+	// Load all instructions.
+	mod.Code = make([]Instruction, 0, 128)
+
+	for {
+		instr, err = dec.DecodeInstruction()
+		if err != nil {
+			if err == io.EOF {
+				break // Not an error -- just end of stream.
+			}
+			return nil, err
+		}
+
+		mod.Code = append(mod.Code, instr)
+	}
+
+	return &mod, nil
+}
+
+// Save writes the module to the given stream.
+func (m *Module) Save(w io.Writer) error {
+	enc := NewEncoder(w)
+
+	// Write the header.
+	err := enc.EncodeHeader(m.Header)
+	if err != nil {
+		return err
+	}
+
+	// Write all instructions.
+	for _, instr := range m.Code {
+		err := enc.EncodeInstruction(instr)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// Verify returns an error if the module contains invalid data.
+// This level of verification applies semantic validation as defined in
+// the specification.
+func (m *Module) Verify() error {
+	// Check the header for structural validity.
+	err := m.Header.Verify()
+	if err != nil {
+		return err
+	}
+
+	// Perform structural validity checks on each instruction, before
+	// we proceed to the semantic checks.
+	for _, instr := range m.Code {
+		err := instr.Verify()
+		if err != nil {
+			return err
+		}
+	}
+
+	// TODO Implement semantic validation.
+	return nil
+}
